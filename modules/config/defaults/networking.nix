5b6b8ec86ef52f11168216935dbb07a2166503e5
Drew Hess <dhess-src@quixoftic.com>
2017-11-22 23:49:22 -0800

    Don't mkForce networking.extraHosts.
    - This is a bit aggressive as a default as there's no way to
      "un-override" it.
    
    - It breaks multi-VM testing, or at the very least makes it a lot more
      difficult.
    
    - It might screw up Kubernetes or container support, which we are
      likely to use in the future.

--- modules/config/defaults/networking.nix
+++ modules/config/defaults/networking.nix
@@ -4,12 +4,6 @@
   config = {
     # We don't use DNSSEC.
     networking.dnsExtensionMechanism = false;
-
-    # We don't use NixOps orchestration, we use real FQDNs, and NixOps's
-    # /etc/hosts modifications screw up IPv6 DNS lookups when using
-    # FQDNs.
-    networking.extraHosts = lib.mkForce "";
-
     networking.firewall.allowPing = true;
   };
 }
