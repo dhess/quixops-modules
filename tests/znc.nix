567f98f1f240db3fc2605cb068cd3d2ea9f19349
Drew Hess <dhess-src@quixoftic.com>
2017-12-03 12:50:06 -0800

    Slightly more robust znc tests.

--- tests/znc.nix
+++ tests/znc.nix
@@ -21,46 +21,100 @@ let
         maintainers = [ dhess ];
       };
 
-      machine = { config, pkgs, ... }:
-      {
-        imports = [
-          ./common/users.nix
-        ] ++ lib.quixopsModules;
-        nixpkgs.overlays = lib.quixopsOverlays;
+      nodes = {
 
-        services.znc = {
-          enable = true;
-          mutable = false;
-          configFile = "/var/run/znc.conf";
-          confOptions = {
-            userName = "bob-znc";
-            nick = "bob";
-            passBlock = "
-              <Pass password>
-                Method = sha256
-                Hash = e2ce303c7ea75c571d80d8540a8699b46535be6a085be3414947d638e48d9e93
-                Salt = l5Xryew4g*!oa(ECfX2o
-              </Pass>
-            ";
+        localhostServer = { config, pkgs, ... }:
+        {
+          imports = [
+          ] ++ lib.quixopsModules;
+          nixpkgs.overlays = lib.quixopsOverlays;
+
+          services.znc = {
+            enable = true;
+            mutable = false;
+            openFirewall = true;
+            configFile = "/var/run/znc.conf";
+            confOptions = {
+              host = "localhost";
+              userName = "bob-znc";
+              nick = "bob";
+              passBlock = "
+                <Pass password>
+                  Method = sha256
+                  Hash = e2ce303c7ea75c571d80d8540a8699b46535be6a085be3414947d638e48d9e93
+                  Salt = l5Xryew4g*!oa(ECfX2o
+                </Pass>
+              ";
+            };
           };
+        };
+
+        server = { config, pkgs, ... }:
+        {
+          imports = [
+          ] ++ lib.quixopsModules;
+          nixpkgs.overlays = lib.quixopsOverlays;
+
+          services.znc = {
+            enable = true;
+            mutable = false;
+            openFirewall = true;
+            configFile = "/var/run/znc.conf";
+            confOptions = {
+              userName = "bob-znc";
+              nick = "bob";
+              passBlock = "
+                <Pass password>
+                  Method = sha256
+                  Hash = e2ce303c7ea75c571d80d8540a8699b46535be6a085be3414947d638e48d9e93
+                  Salt = l5Xryew4g*!oa(ECfX2o
+                </Pass>
+              ";
+            };
+          };
+        };
+
+        client = { config, pkgs, ... }:
+        {
         };
 
       } // machineAttrs;
 
       testScript = { nodes, ... }:
       let
-        zncConf = pkgs.writeText "znc.conf" (modulesLib.mkZncConfig {
+        serverZncConf = pkgs.writeText "znc.conf" (modulesLib.mkZncConfig {
           inherit lib;
-          zncServiceConfig = nodes.machine.config.services.znc;
+          zncServiceConfig = nodes.server.config.services.znc;
+        });
+        localhostServerZncConf = pkgs.writeText "znc.conf" (modulesLib.mkZncConfig {
+          inherit lib;
+          zncServiceConfig = nodes.localhostServer.config.services.znc;
         });
       in
       ''
+        startAll;
+
         # Don't have a good way to synchronize this without NixOps, so
         # we just restart the znc service after the config file has
         # been copied to the server.
-        $machine->copyFileFromHost("${zncConf}", "${nodes.machine.config.services.znc.configFile}");
-        $machine->waitForUnit("znc.service");
+        $server->copyFileFromHost("${serverZncConf}", "${nodes.server.config.services.znc.configFile}");
+        $localhostServer->copyFileFromHost("${localhostServerZncConf}", "${nodes.localhostServer.config.services.znc.configFile}");
+        $server->waitForUnit("znc.service");
+        $localhostServer->waitForUnit("znc.service");
+
+        subtest "no-remote-connections", sub {
+          $client->fail("${pkgs.netcat}/bin/nc -w 5 localhostServer ${builtins.toString nodes.localhostServer.config.services.znc.confOptions.port}");
+        };
+
+        subtest "localhost-connections", sub {
+          $localhostServer->succeed("${pkgs.netcat}/bin/nc -w 5 localhost ${builtins.toString nodes.localhostServer.config.services.znc.confOptions.port}");
+        };
+
+        subtest "allow-remote-connections", sub {
+          $client->succeed("${pkgs.netcat}/bin/nc -w 5 server ${builtins.toString nodes.server.config.services.znc.confOptions.port}");
+        };
       '';
+
     };
 
 in
