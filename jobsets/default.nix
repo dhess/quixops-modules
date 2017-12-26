78c49acd94901f2313913bca3d8fb19acfa19bf3
Drew Hess <dhess-src@quixoftic.com>
2017-12-26 00:55:28 -0800

    Add a simple Hydra declarative jobset.

---
+++ jobsets/default.nix
@@ -0,0 +1,57 @@
+# Based on
+# https://github.com/input-output-hk/iohk-ops/blob/df01a228e559e9a504e2d8c0d18766794d34edea/jobsets/default.nix
+
+{ nixpkgs ? <nixpkgs>
+, declInput ? {}
+}:
+
+let
+
+  quixopsModulesUri = "https://https://github.com/quixoftic/quixops-modules.git";
+
+  mkFetchGithub = value: {
+    inherit value;
+    type = "git";
+    emailresponsible = false;
+  };
+
+  nixpkgs-src = builtins.fromJSON (builtins.readFile ../nixpkgs-src.json);
+
+  pkgs = import nixpkgs {};
+
+  defaultSettings = {
+    enabled = 1;
+    hidden = false;
+    keepnr = 5;
+    schedulingshares = 100;
+    checkinterval = 60;
+    enableemail = false;
+    emailoverride = "";
+  };
+
+  mkQuixopsModules = quixopsModulesBranch: nixpkgsRev: {
+    nixexprpath = "release.nix";
+    nixexprinput = "quixopsModules";
+    description = "QuixOps modules";
+    inputs = {
+      nixpkgs = mkFetchGithub "https://github.com/NixOS/nixpkgs-channels.git ${nixpkgsRev}";
+      qixopsModules = mkFetchGithub "${quixopsModulesUri} ${quixopsModulesBranch}";
+    };
+  };
+
+  mainJobsets = with pkgs.lib; mapAttrs (name: settings: defaultSettings // settings) (rec {
+    quixops-modules = mkQuixopsModules "master" nixpkgs-src.rev;
+  });
+
+  jobsetsAttrs = mainJobsets;
+
+  jobsetJson = pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs);
+
+in {
+  jobsets = with pkgs.lib; pkgs.runCommand "spec.json" {} ''
+    cat <<EOF
+    ${builtins.toJSON declInput}
+    EOF
+    cp ${jobsetJson} $out
+  '';
+}
