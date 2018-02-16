4d1c51a90c659a5aa23d47dafb9544382f63459d
Drew Hess <dhess-src@quixoftic.com>
2018-02-15 23:20:46 -0800

    Disable dhparams for Nginx config.
    Note that, per our Mozilla-recommended "modern" SSL ciphers, we don't
    need dhparams, because all of the ciphers defined here use ECDHE.

--- modules/config/defaults/nginx.nix
+++ modules/config/defaults/nginx.nix
@@ -27,16 +27,6 @@ in
       settings.
     '';
 
-    dhparamsSize = mkOption {
-      type = types.int;
-      default = 3072;
-      example = 2048;
-      description = ''
-        If <option>enable</option> is <literal>true</literal>, the
-        nginx DH parameter size in bits is set by this option.
-      '';
-    };
-
   };
 
   config = mkIf enabled {
@@ -53,17 +43,7 @@ in
       sslCiphers = "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256";
 
       sslProtocols = "TLSv1.2";
-      sslDhparam = "${config.security.dhparams.path}/nginx.pem";
     };
-
-
-    # Only enable dhparams if nginx is also enabled; otherwise, every
-    # host that enables our top-level default settings (which we
-    # recommend) will waste time generating dhparams for a service
-    # they haven't enabled.
-
-    security.dhparams.params = { nginx = cfg.dhparamsSize; };
-    security.dhparams.enable = nginx_enabled;
 
   };
 
