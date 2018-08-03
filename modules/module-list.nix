07ee97e5b7eba7da75c452563bf21a0856ad6713
Drew Hess <dhess-src@quixoftic.com>
2018-08-03 01:10:53 -0700

    Add support for AMD Jaguar platforms (incl. apu3c4).

--- modules/module-list.nix
+++ modules/module-list.nix
@@ -11,6 +11,9 @@
   ./config/defaults/tmux.nix
   ./config/defaults/users.nix
   ./config/services/fail2ban.nix
+  ./config/hardware/amd/common.nix
+  ./config/hardware/amd/jaguar.nix
+  ./config/hardware/apu2/apu3c4.nix
   ./config/hardware/intel/broadwell-de.nix
   ./config/hardware/intel/centerton.nix
   ./config/hardware/intel/common.nix
