c98b6304b802a82f6c30486f1ac358d88c19877f
Drew Hess <dhess-src@quixoftic.com>
2018-10-14 01:15:47 -0400

    nginx defaults: use sslModernCiphers from nixpkgs-lib-quixoftic.

--- modules/config/defaults/nginx.nix
+++ modules/config/defaults/nginx.nix
@@ -41,7 +41,7 @@ in
       ## Mozilla recommendations. See
       ## https://mozilla.github.io/server-side-tls/ssl-config-generator/?server=nginx-1.10.3&openssl=1.0.1e&hsts=yes&profile=modern
 
-      sslCiphers = "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256";
+      sslCiphers = pkgs.lib.security.sslModernCiphers;
 
       sslProtocols = "TLSv1.2";
 
